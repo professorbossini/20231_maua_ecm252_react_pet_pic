70070a69b361de91fff8907d11411b156ff4f866
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-13 11:32:06 -0300

    apostila07(pet-pic): fim da secao 2.5

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Busca from './Busca'
 //useState é um hook (a partir da versão 16.8)
 
 const App = () => {
   return (
-    <div>
-      <h1>Exibir uma lista de...</h1>
+    <div 
+      className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
+        <div className="col-12">
+          <h1>Exibir uma lista de...</h1>
+        </div>
+        <div className="col-8">
+          <Busca />
+        </div>
     </div>
   )
 }
--- src/components/Busca.js
+++ src/components/Busca.js
@@ -3,21 +3,32 @@ import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
 const Busca = (props) => {
   const [termoDeBusca, setTermoDeBusca] = useState('')
+  const onTermoAlterado = (event) => {
+    setTermoDeBusca(event.target.value)
+  }
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+  }
   return (
-    <div className="flex flex-column">
-      {/* span.p-input-icon-left>i.pi.pi-search */}
-      <span className="p-input-icon-left w-full">
-        <i className="pi pi-search"></i>
-        <InputText 
-          className='w-full'
-          placeholder={props.dica}
+    <form onFormSubmit={onFormSubmit}>
+      <div className="flex flex-column">
+        {/* span.p-input-icon-left>i.pi.pi-search */}
+        <span className="p-input-icon-left w-full">
+          <i className="pi pi-search"></i>
+          <InputText 
+            className='w-full'
+            placeholder={props.dica}
+            onChange={onTermoAlterado}
+            value={termoDeBusca}
+          />
+        </span>
+        <Button 
+          label='OK'
+          className='p-button-outlined mt-2'
         />
-      </span>
-      <Button 
-        label='OK'
-        className='p-button-outlined mt-2'
-      />
-    </div>
+      </div>
+    </form>
   )
 }
 
